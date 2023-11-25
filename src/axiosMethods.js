b67b35beda0c42c5d9c75b64823bad052d43294c
Mayuresan <87636548+Mayuresan08@users.noreply.github.com>
2023-11-25 14:20:26 +0530

    Update axiosMethods.js

--- src/axiosMethods.js
+++ src/axiosMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://makeyouup.herokuapp.com/";
+const BASE_URL = "https://ecommercebackend-o0yl.onrender.com";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
